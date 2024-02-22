4c1a15d0bf0f8ba562b6bf9d125fefef19537586
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2024-02-22 15:24:41 +0100

    Body jedou

---
+++ Grafika_IT2/Board.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Grafika_IT2
+{
+  public class Board
+  {
+    private List<Point> points;
+
+    public Board()
+    {
+      points = new List<Point>();
+    }
+
+    public void Clear()
+    {
+      points.Clear();
+    }
+
+    public void Add(Point point)
+    {
+      points.Add(point);
+    }
+
+    public void Draw(Canvas canvas)
+    {
+      canvas.Children.Clear();
+      foreach (var point in points)
+      {
+        Ellipse ellipse = new Ellipse();
+        ellipse.Width = 25;
+        ellipse.Height = 25;
+        ellipse.Fill = Brushes.Blue;
+        Canvas.SetLeft(ellipse, point.X);
+        Canvas.SetTop(ellipse, point.Y);
+        canvas.Children.Add(ellipse);
+      }
+    }
+  }
+}
--- Grafika_IT2/MainWindow.xaml.cs
+++ Grafika_IT2/MainWindow.xaml.cs
@@ -20,9 +20,18 @@ namespace Grafika_IT2
   /// </summary>
   public partial class MainWindow : Window
   {
+    private Board board;
+
     public MainWindow()
     {
       InitializeComponent();
+      board = new Board();
+    }
+
+    private void canvasBoard_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+      board.Add(e.GetPosition(canvasBoard));
+      board.Draw(canvasBoard);
     }
   }
 }
